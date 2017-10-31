a7cbe1bc1993b3b5ee45164121829b2c9b4b8223
thompson <thauer@edu.aau.at>
2017-10-31 11:11:14 +0100

    > RegexDomainName

--- Abgabe_1/ab1/impl/Auer_Siljic/RegExImpl.java
+++ Abgabe_1/ab1/impl/Auer_Siljic/RegExImpl.java
@@ -1,18 +1,23 @@
 package ab1.impl.Auer_Siljic;
 
 import ab1.RegEx;
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
 public class RegExImpl implements RegEx {
 
+	/**
+	 * Domain-Name ohne Umlaute und Sonderzeichen (vereinfachte Form). Nur
+	 * Kleinbuchstaben. TLD mit 2 oder 3 Zeichen.
+	 * Sample: http://www.pcwebopedia.com/index.html, the domain name is pcwebopedia.com
+	 */
 	@Override
 	public String getRegexDomainName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "[a-z]*.((at)|(com)|(eu))";	// TODO: Needs extension pending on tests
 	}
 
 	@Override
 	public String getRegexEmail() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
