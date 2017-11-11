6f6b437a0b90b0e157896fca190eda5589be29a7
thompson <thauer@edu.aau.at>
2017-11-11 20:03:13 +0100

    > Fix RegexDomainName >> Could be done better

--- Abgabe_1/ab1/impl/Auer_Siljic_Harden/RegExImpl.java
+++ Abgabe_1/ab1/impl/Auer_Siljic_Harden/RegExImpl.java
@@ -7,46 +7,51 @@ public class RegExImpl implements RegEx {
 	/**
 	 * Domain-Name ohne Umlaute und Sonderzeichen (vereinfachte Form). Nur
 	 * Kleinbuchstaben. TLD mit 2 oder 3 Zeichen.
-	 * Sample: http://www.pcwebopedia.com/index.html, the domain name is pcwebopedia.com
+	 *
 	 */
 	@Override
 	public String getRegexDomainName() {
-		return "[a-z]*.((at)|(com)|(eu))";	// TODO: Needs extension pending on tests
+
+		return "([w]{3}(\\.){0,1}[a-z]*(\\.){0,1}\\.[a-z]{2,3})|([a-z]*)\\.[a-z]{2,3}|([a-z]*\\.[a-z]*)\\.[a-z]{2,3}";
 	}
 
 	@Override
 	public String getRegexEmail() {
-        // TODO Auto-generated method stub
         return "([a-zA-Z0-9.-]*)\\@(([a-z]*)(.*))";
     }
 
 	@Override
 	public String getRegexURL() {
-		// TODO Auto-generated method stub
-		return null;
+		return "([http|https|ftp])://"+getRegexDomainName()+"[0-9]{4}";
 	}
 
 	@Override
 	public String multiMatch1() {
-		// TODO Auto-generated method stub
         return "[a-z]*(foo)[a-z]*";
     }
 
 	@Override
 	public String multiMatch2() {
-		// TODO Auto-generated method stub
         return "([a-z]*|[A-Z]*)(ick)";
     }
 
 	@Override
 	public String multiMatch3() {
-		// TODO Auto-generated method stub
         return "(([a-z]*)(fu))";
     }
 
+	/**
+	  Geben Sie eine RegEx an, die folgende Zeichenketten erkennt: \w+, \w*,
+	  a{1}|a{3}|a{9}, \???, a{1,8}, -, -+-+-+-+-+, (\w+?)\7, (\ufacdf)*?,
+	  [A-Z0-9]+, [a-fk-ov-z]*, (?:[aeiou]+)\1+, [a\-z\-9], (\001)\1, (\2)\1
+
+	  Folgende Zeichenketten sollen nicht erkannt werden: \w++, \w**, a({1}|{3}|{9}), ???,
+	  a{8,1}, +, +-+-+-+-+-, (\w?+)\7, (\ufacdf)?*, [Z-A0-9]+, [f-bp-iy-t]*,
+	  (:?[abced]+)\1*, [a-z-9], (\1)\1, (\8)\2
+	*/
 	@Override
 	public String multiMatch4() {
-		// TODO Auto-generated method stub
+//		return "((w(\+|\*)))|(-[+-]*)|(((a{[0-9]})|(a{[0-9]})|(a{0-9})))|(\\\?\?\?)|(a{[0-9]\,[0-9]})|((\(\\w\+\?\))\\[0-9])|(\(\\[a-zA-Z]*\)\*\?)|(\[A\-Z0\-9\]\+)|(\(\\[0-9][0-9][0-9]\)\\)";
 		return null;
 	}
 }
