4b9c26995c8bbc437e527397331c4675d2fdcfda
thompson <thauer@edu.aau.at>
2017-11-17 11:31:02 +0100

    > Minor Corrections >> Fml that URL

--- Abgabe_1/ab1/impl/Auer_Siljic_Harden/RegExImpl.java
+++ Abgabe_1/ab1/impl/Auer_Siljic_Harden/RegExImpl.java
@@ -6,18 +6,24 @@ public class RegExImpl implements RegEx {
 
 	@Override
 	public String getRegexDomainName() {
-		return "([w]{3}(\\.){0,1}[a-z]*(\\.){0,1}\\.[a-z]{2,3})|([a-z]*)\\.[a-z]{2,3}|([a-z]*\\.[a-z]*)\\.[a-z]{2,3}";
+		return "([w]{3}(\\.)?[a-z]+(\\.)?\\.[a-z]{2,3})|([a-z]+)\\.[a-z]{2,3}|([a-z]+\\.[a-z]+)\\.[a-z]{2,3}";
 	}
 
 	@Override
 	public String getRegexEmail() {
-		return "([a-z]+@[a-z]*\\.[a-z]{2,3})|([a-z]+\\.[a-z]+@[a-z]+\\.[a-z]{2,3})|([a-z]+\\-[a-z]+@[a-z]+\\.[a-z]{2,3})";
+		return "([a-z]+[a-z.]*@[a-z]*\\.[a-z]{2,3})|([a-z]+\\.[a-z]+@[a-z]+\\.[a-z]{2,3})|([a-z]+\\-[a-z]+@[a-z]+\\.[a-z]{2,3})";
     }
 
+	/**
+	 * Ein URL, welches die Regeln 1-4 erfüllt. (1) Muss mit http, https oder ftp
+	 * starten und danach ein :// enthalten. (2) Muss eine gülte Domain besitzen (3)
+	 * Kann eine Portnummer enthalten (4) Kann Ziffern, Buchstaben, Punkte,
+	 * Bindestriche und Schrägstriche enthalten
+	 */
 	@Override
 	public String getRegexURL() {
+		return "(http)|(https)|(https)://[a-zA-Z0-9.-/]+";
 		//return "(((http)|(https)|(ftp))\\:\\/\\/[a-z]+\\.[a-z]+(\\.[a-z]+)?(\\:[0-9]+\\/?)?([a-z]+(((.)|(\\/))?)([a-z]+)?){0,})";
-		return null;
 	}
 
 	@Override
@@ -30,29 +36,15 @@ public class RegExImpl implements RegEx {
         return "^[a-zA-Z]+(ick)$";
     }
 
-	/**
-	 * Geben Sie eine RegEx an, die folgende Zeichenketten erkennt: fu, tofu, snafu
-	 *
-	 * Folgende Zeichenketten sollen nicht erkannt werden: futz, fusillade, functional,
-	 * discombobulated
-	 */
 	@Override
 	public String multiMatch3() {
         return "^[a-z]*(fu)$";
     }
 
-	/**
-	  Geben Sie eine RegEx an, die folgende Zeichenketten erkennt: \w+, \w*,
-	  a{1}|a{3}|a{9}, \???, a{1,8}, -, -+-+-+-+-+, (\w+?)\7, (\ufacdf)*?,
-	  [A-Z0-9]+, [a-fk-ov-z]*, (?:[aeiou]+)\1+, [a\-z\-9], (\001)\1, (\2)\1
 
-	  Folgende Zeichenketten sollen nicht erkannt werden: \w++, \w**, a({1}|{3}|{9}), ???,
-	  a{8,1}, +, +-+-+-+-+-, (\w?+)\7, (\ufacdf)?*, [Z-A0-9]+, [f-bp-iy-t]*,
-	  (:?[abced]+)\1*, [a-z-9], (\1)\1, (\8)\2
-	*/
+
 	@Override
 	public String multiMatch4() {
-//		return "((w(\+|\*)))|(-[+-]*)|(((a{[0-9]})|(a{[0-9]})|(a{0-9})))|(\\\?\?\?)|(a{[0-9]\,[0-9]})|((\(\\w\+\?\))\\[0-9])|(\(\\[a-zA-Z]*\)\*\?)|(\[A\-Z0\-9\]\+)|(\(\\[0-9][0-9][0-9]\)\\)";
-		return null;
+		return "";
 	}
 }
