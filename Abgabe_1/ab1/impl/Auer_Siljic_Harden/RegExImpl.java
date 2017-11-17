9d05fd2edc729d75be45372b9b21076d1223ad97
igorharden <i.harden@gmx.at>
2017-11-17 16:23:45 +0100

    \w++ darf nicht matchen matcht aber

--- Abgabe_1/ab1/impl/Auer_Siljic_Harden/RegExImpl.java
+++ Abgabe_1/ab1/impl/Auer_Siljic_Harden/RegExImpl.java
@@ -1,6 +1,7 @@
 package ab1.impl.Auer_Siljic_Harden;
 
 import ab1.RegEx;
+import java.io.*;
 
 public class RegExImpl implements RegEx {
 
@@ -15,14 +16,14 @@ public class RegExImpl implements RegEx {
     }
 
 	/**
-	 * Ein URL, welches die Regeln 1-4 erfüllt. (1) Muss mit http, https oder ftp
+	 * Ein URL | welches die Regeln 1-4 erfüllt. (1) Muss mit http | https oder ftp
 	 * starten und danach ein :// enthalten. (2) Muss eine gülte Domain besitzen (3)
-	 * Kann eine Portnummer enthalten (4) Kann Ziffern, Buchstaben, Punkte,
+	 * Kann eine Portnummer enthalten (4) Kann Ziffern | Buchstaben | Punkte,
 	 * Bindestriche und Schrägstriche enthalten
 	 */
 	@Override
 	public String getRegexURL() {
-		return "(http)|(https)|(https)://[a-zA-Z0-9.-/]+";
+		return "((http)|(https)|(ftp)):\\/\\/[a-zA-Z0-9.\\-\\/]+(:[0-9]+)?((\\/)?|(\\/[a-zA-Z0-9.\\-\\/]+)?)";
 		//return "(((http)|(https)|(ftp))\\:\\/\\/[a-z]+\\.[a-z]+(\\.[a-z]+)?(\\:[0-9]+\\/?)?([a-z]+(((.)|(\\/))?)([a-z]+)?){0,})";
 	}
 
@@ -45,6 +46,30 @@ public class RegExImpl implements RegEx {
 
 	@Override
 	public String multiMatch4() {
-		return "";
+
+		String str = "";
+		try {
+			FileReader readerOfDespair = new FileReader("/home/igor/Misc/Introduction-to-theoretical-Computer-Science/Abgabe_1/ab1/impl/Auer_Siljic_Harden/ultraregex");
+//			FileReader readerOfDespair = new FileReader("ab1/impl/Auer_Siljic_Harden/ultraregex");
+			BufferedReader bufferedFailer = new BufferedReader(readerOfDespair);
+
+            str = bufferedFailer.readLine();
+
+            readerOfDespair.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+//		Regex r = "(\\w(\\+|\\*)|(a\{[0-9]\}(\|)?)+";
+//	    String str =
+//		String str = "\\w\\+ | \\w* | a{1}|a{3}|a{9} | \\??? | a{1,8} | - | -+-+-+-+-+ | " +
+//				"(\\w+?)\\7 | (\\ufacdf)*? | [A-Z0-9]+ | [a-fk-ov-z]* | (?:[aeiou]+)\\1+" +
+//				"| [a\\-z\\-9] | (\\001)\\1 | (\\2)\\1";
+
+//		String str = "\\w\\+ | \\w* | a{1}|a{3}|a{9} | \\??? | a{1,8} | - | -+-+-+-+-+ | " +
+//				"(\\w+?)\\7 | (\\ufacdf)*? | [A-Z0-9]+ | [a-fk-ov-z]* | (?:[aeiou]+)\\1+" +
+//				"| [a\\-z\\-9] | (\\001)\\1 | (\\2)\\1";
+		return str;
 	}
 }
