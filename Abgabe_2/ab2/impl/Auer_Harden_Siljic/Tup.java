ba8f9c90ce1ba0793dc9cf44f40e0539e115cf0f
igorharden <i.harden@gmx.at>
2017-12-28 15:49:53 +0100

    RSA-Implementation(Minimierung)

---
+++ Abgabe_2/ab2/impl/Auer_Harden_Siljic/Tup.java
@@ -0,0 +1,11 @@
+package ab2.impl.Auer_Harden_Siljic;
+
+public class Tup {
+    public int left;
+    public int right;
+
+    public Tup(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+}
