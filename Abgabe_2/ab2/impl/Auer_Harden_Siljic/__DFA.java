43322dee6fd74b1907a9eb6a6df4881345146600
thompson <thauer@edu.aau.at>
2018-01-30 18:59:05 +0100

    DFA

--- Abgabe_2/ab2/impl/Auer_Harden_Siljic/__DFA.java
+++ Abgabe_2/ab2/impl/Auer_Harden_Siljic/__DFA.java
@@ -1,7 +1,6 @@
 package ab2.impl.Auer_Harden_Siljic;
 
 import ab2.DFA;
-import ab2.NFA;
 import ab2.fa.exceptions.IllegalCharacterException;
 
 import java.util.Set;
@@ -26,34 +25,50 @@ public class __DFA extends __NFA implements DFA{
     @Override
     public int doStep(char c) throws IllegalArgumentException, IllegalStateException {
         if(!getSymbols().contains(c)) throw new IllegalArgumentException();
+        int nextState = -1;
 
         for(int i = 0; i < getTransitions().length; i++) {
             for(int j = 0; j < getTransitions()[0].length; j++){
-                if(getTransitions()[i][j].contains(c)){
+                if(getTransitions()[i][j].contains(c+"")){
                     // IF is redundant as fucking fuck
-                    if(j < getNumStates() || j > getNumStates()) throw new IllegalStateException();
-                    else actState = j;
+                    nextState = j;
                 }
             }
         }
 
-
-        return 0;
+        if(nextState < 0 || nextState > getNumStates()) throw new IllegalStateException();
+        return nextState;
     }
 
     @Override
     public Integer getNextState(int s, char c) throws IllegalCharacterException, IllegalStateException {
-        return null;
+        if(!getSymbols().contains(c)) throw new IllegalCharacterException();
+        if(s < getNumStates() || s > getNumStates()) throw new IllegalStateException();
+
+        // FIXME brainfucked right now
+        int nextState = -1;
+        for(int i = 0; i < getTransitions().length; i++){
+           if(getTransitions()[s][i].contains(c+"")){
+               nextState = i;
+           }
+        }
+
+        return nextState;
     }
 
     @Override
     public boolean isAcceptingState() {
-        return false;
+        if(getAcceptingStates().contains(actState)) return true;
+        else return false;
     }
 
 
     @Override
     public void setTransition(int fromState, char c, int toState) throws IllegalStateException, IllegalCharacterException {
+        if(fromState < getNumStates() || fromState > getNumStates() ||
+                toState < getNumStates() || toState > getNumStates()) throw new IllegalStateException();
+        if(!getSymbols().contains(c+"")) throw new IllegalCharacterException();
 
+        setTransition(fromState, c+"", toState);
     }
 }
